33d21b55e5c644a8b21ea6136c1adf490a096928
ahastuart <qusdkgus1224@naver.com>
2025-01-03 16:56:15 +0900

    cloud-demo main,devops

--- phpdemo/user-data.sh
+++ phpdemo/user-data.sh
@@ -6,9 +6,12 @@ export DB_URL=${db_url}
 sed -i "s/DB_IP/$DB_URL/g" cloud-demo/process_create.php
 
 export DB_USER=${db_username}
-sed -i "s/root/$DB_USER/g" cloud-demo/process_create.php
+sed -i "s/USERNAME/$DB_USER/g" cloud-demo/process_create.php
 
 export DB_PASS=${db_password}
-sed -i "s/Test123!/$DB_PASS/g" cloud-demo/process_create.php
+sed -i "s/PASSWORD/$DB_PASS/g" cloud-demo/process_create.php
+
+export DB_NAME=${db_name}
+sed -i "s/DBNAME/$DB_NAME/g" cloud-demo/process_create.php
 
 cp cloud-demo/*.php /var/www/html
